136126ecc867a22fba31ff2112e381ae27aa547f
Shraddhey <siddhu1101@gmail.com>
2020-12-11 10:32:48 +0530

    Code till MVC

---
+++ models/product.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const filePath = require('../util/path');
+const path = require('path');
+
+const p = path.join(filePath, 'data', 'products.json');
+
+const getProductsFromFile = (cb) => {
+    fs.readFile(p, (err, fileContent) => {
+        if (err) {
+            return cb([]);
+        }
+        return cb(JSON.parse(fileContent));
+    });
+}
+
+module.exports = class Product {
+    constructor(title) {
+        this.title = title;
+    }
+
+    save() {
+        getProductsFromFile(products => {
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), (err) => {
+                console.log(err);
+            });
+        });
+    }
+
+    static fetchAll(cb) {
+        getProductsFromFile(cb);
+    }
+}
